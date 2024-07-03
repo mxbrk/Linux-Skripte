12a7d71bc5fd1f8a6aa3672ae69f4af1aca6a062
Maximilian Bronkhorst <maximilian.bronkhorst@protonmail.com>
2024-07-03 09:29:02 +0200

    cleaned up

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Systemaktualisierung wird gestartet" 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean -y
 sudo apt clean -y
-echo "Fertig..." 
+echo "Fertig" 
